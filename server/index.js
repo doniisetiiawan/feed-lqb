4704debd0acc20246ee8879cd97524e4cb74ff32
Doni Setiawan <donisetiawan.050@gmail.com>
2020-08-03 14:55:31 +0700

    Enable SSR

---
+++ server/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies */
+require('@babel/polyfill');
+
+require('@babel/register')({
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+  plugins: ['@babel/plugin-proposal-class-properties'],
+});
+
+require('./server');
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+ReactDOM.hydrate(
   // eslint-disable-next-line react/jsx-filename-extension
   <>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </>,
   document.getElementById('root'),
 );
